fb3c216b00d8d3a2021789c0381724bce86bae3a
Dave Hayes <dwhayes@gmail.com>
2015-05-29 07:48:08 -0500

    Use humanize_bytes

--- powerline/segments/common/disk.py
+++ powerline/segments/common/disk.py
@@ -4,22 +4,13 @@ from __future__ import (
     unicode_literals, division, absolute_import, print_function)
 
 import psutil
-
-
-def format_number(number):
-    """Format bytes into something human readable."""
-    units = ['B', 'KiB', 'MiB', 'GiB', 'TiB']
-    for unit in units:
-        if number < 1024:
-            break
-        number = number / 1024
-    return '%.1f %s' % (number, unit)
+from powerline.lib.humanize_bytes import humanize_bytes
 
 
 def disk_free(pl):
     """Return the free disk space segment."""
     total, used, free, percent = psutil.disk_usage('/')
-    contents = format_number(free)
+    contents = humanize_bytes(free, 'B')
     return [{
         'contents': contents,
         'highlight_groups': ['weather_temp_gradient'],
